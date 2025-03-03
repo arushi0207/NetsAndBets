2d37a4a9a4191e1fefb46243ba559c5ab9bf5cd8
Kevin Yuan <kyuan26@wisc.edu>
2025-03-03 14:11:37 -0600

    documentation

--- demo/src/main/java/com/example/demo/controller/HelloController.java
+++ demo/src/main/java/com/example/demo/controller/HelloController.java
@@ -10,17 +10,41 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+/**
+ * REST controller for handling message-related requests.
+ *
+ * <p>This controller provides endpoints to save and retrieve messages using
+ * a {@link MessageRepository}.
+ * The @RestController annotation indicates that this class will handle HTTP requests and return JSON responses.</p>
+ *
+ * <p>It exposes the following endpoints:</p>
+ * <ul>
+ *   <li>POST {@code /hello} - Saves a new message.</li>
+ *   <li>GET {@code /hello} - Retrieves all stored messages.</li>
+ * </ul>
+ */
 @RestController
 public class HelloController {
 
-    @Autowired
+    @Autowired // an annotation used for automatic dependency injection
     private MessageRepository messageRepository;
 
+    /**
+     * Saves a new message.
+     *
+     * @param message The message to be saved, provided in the request body.
+     * @return The saved {@link Message} entity.
+     */
     @PostMapping("/hello")
     public Message saveMessage(@RequestBody Message message) {
         return messageRepository.save(message);
     }
 
+    /**
+     * Retrieves all messages in {@link MessageRepository}.
+     *
+     * @return A list of all stored {@link Message} entities.
+     */
     @GetMapping("/hello")
     public List<Message> getMessages() {
         return messageRepository.findAll();
