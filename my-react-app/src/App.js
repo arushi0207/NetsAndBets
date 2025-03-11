6bb18cd76f2470165152531fd96d7dc786da6821
Aditya Mehrotra <adi1.mehrotra@gmail.com>
2025-03-11 08:10:17 -0500

    Modified App.js to include a link to the home page.

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Login from './Login.js';
+import Home from './Home';
+import Login from './Login';
 import Signup from './SignUp';
 
 function App() {
   return (
     //Define application routes to handle navigation between various pages
+    <Router>
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
       </Routes>
+    </Router>
   );
 }
 
